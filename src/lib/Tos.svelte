f059009e656a8d635e5ca07d559f673c3e3dc68b
Rioni <nrosa01@ucm.es>
2023-06-08 17:39:33 +0200

    TOS Layout updated

--- src/lib/Tos.svelte
+++ src/lib/Tos.svelte
@@ -2,104 +2,89 @@
   import BackButton from "./BackButton.svelte";
 </script>
 
-<div class="text-center justify-center">
-  <h2 class="title-bold"><strong>Terms of service</strong></h2>
-  <p class="paragraph-light">
-    <span class="p"
-      >This is subject to change at any given point with no prior notice or
-      possibility to claim.<br />
-      By commissioning me, Rioni, you agree to my Terms of Service.</span>
-  </p>
-  <div>
-    <div class="flex flex-cols justify-evenly flex-wrap">
-      <div>
-        <h2 class="title pb-8">Will draw</h2>
-        <p>
-          <span class="paragraph"
-            >Pokemon OCs or Fanart<br />
-            Digimon OCs or Fanart<br />
-            Furry / Fursonas<br />
-            Any pokemon like creature<br />
-            Post TF themed art<br />
-            Humans*<br />
-            Backgrounds<br />
-            Up to 5 characters</span>
-        </p>
-      </div>
-      <div>
-        <h2 class="title pb-8">Won&#39;t draw</h2>
-        <p>
-          <span class="paragraph"
-            >NSFW<br />
-            Mecha<br />
-            Gore<br />
-            Religious, political, ideological symbols<br />
-            Complex clothes</span>
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="flex flex-cols justify-evenly flex-wrap">
-    <div>
-      <h2 class="title pb-8 mt-8">Process</h2>
-      <p>
-        <span class="paragraph">
-          One or many rough sketches will be sent.<br />
-          Once the sketch is approved, payment must be done*.<br 
-          />If you take advantage of this to get free sketches I'll blacklist you.<br />
-          I'll send WIPs during the process (after sketch and line).<br />
-          Changes can be requested before lineart is done.<br />
-          I won't do more than 4 changes.<br />
-          Asking for many changes may incur into additional charges.<br />
-          *When payment time comes, I'll send you a paypal.me link.</span>
-      </p>
-    </div>
-    <div>
-      <h2 class="title pb-8 mt-8">Other information</h2>
-      <p>
-        <span class="paragraph">
-          ETA: 1-2 weeks<br />
-          Non-commercial use<br />
-          I save the right to post your drawing online as well as on my web.<br />
-          If you don't want your drawing to be public you must tell me.<br />
-          I need a 2D ref, if possible unshaded.</span>
-      </p>
-    </div>
-    <div>
-      <h2 class="title pb-8 mt-8">How to order</h2>
-      <p>
-        <span class="paragraph">
-          Send me a DM (<a class="link" href="https://twitter.com/rionisguild">Twitter</a> or <a class="link" href="https://discordapp.com/users/490599226200424448">Discord</a>), or mail <a class="link" href="mailto:business@rioni.es ">Rioni</a> with the following information:<br />
-      </p>
-      <p class="pt-4 flex flex-cols justify-evenly">
-        <span class="paragraph">
-          Commission type<br />
-          Character ref<br />
-          Pose / Drawing description<br />
-          Background (if any)</span>
-      </p>
-
-      <p class="paragraph-light xl:text-base text-sm !py-0">
-        The more refs and info you provide, the better.<br />
-        But of course if you don't have a clear idea
-        of what you want, say you want a post tf art of your character<br>
-        but you don't know the pose, reaction or setting. I'll try to
-        come up with something that suits your character.
-      </p>
-      
-    </div>
-</div>
-    <p class="flex flex-col pt-8">
-      <span class="py-4"
-        ><span class="title">Additional information</span></span
-      ><span class="paragraph-light xl:pb-4 py-2"
-        >Complex designs can incur into additional charges</span
-      ><span class="paragraph-light !py-0"
-        >*I can draw humans, but I&#39;m not really experienced with it, for
-        post tf art I can draw headshots.<br>My struggles are complex body poses
-        and hands. I prefer to avoid drawings humans now except for headshots.</span>
+<div class="py-12">
+  <div class="container mx-auto px-4">
+    <h2 class="title-bold text-center mb-8">Terms of Service</h2>
+    <p class="text-center text-white text-sm mb-8 quicksand">
+      This is subject to change at any given point with no prior notice or possibility to claim. By commissioning me, Rioni, you agree to my Terms of Service.
     </p>
+
+    <ul class="flex flex-wrap text-center quicksand">
+      <li class="flex-grow xl:w-1/2 md:w-full xl:px-2 md:px-4 mb-8">
+        <div class="xl:py-8 px-8 py-4">
+          <h2 class="subtitle text-center mb-8">Will Draw</h2>
+          <ul class=" text-white text-sm">
+            <li>Pokemon OCs or Fanart</li>
+            <li>Digimon OCs or Fanart</li>
+            <li>Furry / Fursonas</li>
+            <li>Any pokemon-like creature</li>
+            <li>Post TF themed art</li>
+            <li>Humans*</li>
+            <li>Backgrounds</li>
+            <li>Up to 5 characters</li>
+          </ul>
+        </div>
+      </li>
+
+      <li class="flex-grow xl:w-1/2 md:w-full xl:px-2 md:px-4 mb-8">
+        <div class="xl:py-8 px-8 py-4">
+          <h2 class="subtitle text-center xl:mb-8 xl:mb-8 mb-4">Won't Draw</h2>
+          <ul class=" text-white text-sm">
+            <li>NSFW</li>
+            <li>Mecha</li>
+            <li>Gore</li>
+            <li>Religious, political, ideological symbols</li>
+            <li>Complex clothes</li>
+          </ul>
+        </div>
+      </li>
+
+      <li class="flex-grow w-full xl:w-1/2 xl:px-2 md:px-4 mb-8">
+        <div class="xl:py-8 px-8 py-4">
+          <h2 class="subtitle text-center mb-8">How to Order</h2>
+          <p class="text-white text-sm xl:mb-8 mb-4">
+            Send me a DM (<a href="https://twitter.com/rionisguild" class="link">Twitter</a> or <a href="https://discordapp.com/users/490599226200424448" class="link">Discord</a>), or mail <a href="mailto:business@rioni.es" class="link">Rioni</a> with the following information:
+          </p>
+          <ul class=" text-white text-sm">
+            <li>Commission type</li>
+            <li>Character ref</li>
+            <li>Pose / Drawing description</li>
+            <li>Background (if any)</li>
+          </ul>
+          <p class="text-white text-sm mt-4">
+            The more refs and info you provide, the better. But of course, if you don't have a clear idea of what you want, say you want a post tf art of your character but you don't know the pose, reaction, or setting. I'll try to come up with something that suits your character.
+          </p>
+        </div>
+      </li>
+
+      <li class="flex-grow w-full xl:w-1/2 xl:px-2 md:px-4 mb-8">
+        <div class="xl:py-8 px-8 py-4">
+          <h2 class="subtitle text-center mb-8">Process</h2>
+          <p class="text-white text-sm">
+            One or many rough sketches will be sent. Once the sketch is approved, payment must be done*. If you take advantage of this to get free sketches, I'll blacklist you. I'll send WIPs during the process (after sketch and line). Changes can be requested before lineart is done. I won't do more than 4 changes. Asking for many changes may incur into additional charges. *When payment time comes, I'll send you a paypal.me link.
+          </p>
+        </div>
+      </li>
+
+      <li class="flex-grow w-full xl:w-1/2 xl:px-2 md:px-4">
+        <hr class="mt-2 mb-4 h-0.5 border-t-0 bg-neutral-100/10" />
+        <div class="xl:py-8 px-8 py-4">
+          <h2 class="subtitle text-center mb-8">Other Information</h2>
+          <p class="text-white text-sm xl:mb-8 mb-4">
+            ETA: 1-2 weeks<br />
+            Non-commercial use<br />
+            I save the right to post your drawing online as well as on my web. If you don't want your drawing to be public, you must tell me. I need a 2D ref, if possible unshaded.
+          </p>
+          <p class="text-white text-sm">
+            Complex designs can incur into additional charges.<br />
+            *I can draw humans, but I'm not really experienced with it. For post tf art, I can draw headshots. My struggles are complex body poses and hands. I prefer to avoid drawing humans now except for headshots.
+          </p>
+        </div>
+      </li>
+    </ul>
+
+    <div class="w-full flex justify-center">
+      <BackButton />
+    </div>
   </div>
-  <div class="w-full flex justify-center">
-    <BackButton />
-  </div>
+</div>
