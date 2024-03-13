9b51eb3d620b90418076970153694831b1f9057a
Rioni <nrosa01@ucm.es>
2024-03-13 18:14:59 +0100

    Deleted project

--- src/components/SvelteGallery/index.js
+++
@@ -1,4 +0,0 @@
-import "./app.postcss";
-import { default as SvelteGallery } from "./App.svelte";
-
-export default SvelteGallery;
--- src/components/SvelteGallery/lib/BackButton.svelte
+++
@@ -1,3 +0,0 @@
-<a href="#home" class="flex justify-center items-center text-white font-semibold hover:bg-[#ff8ca8] text-sm py-2 px-4 bg-[#FFFFFF2B] hover:border-[#ff8ca8] rounded w-32 h-11 uppercase mt-8">
-    Back
-</a>
